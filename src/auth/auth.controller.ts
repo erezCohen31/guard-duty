4cf865dac64558f380bcbdd8c229fff1f8f8dfab
erez cohen <erezjbh@gmail.com>
2025-07-29 17:39:07 +0300

    fix the package json

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post, HttpCode, HttpStatus, Get, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    signIn(@Body() loginDto: LoginDto) {
+        return this.authService.signIn(loginDto.email, loginDto.password);
+    }
+
+    @Post('register')
+    register(@Body() registerDto: RegisterDto) {
+        return this.authService.register(registerDto);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
+    }
+}
